fec044960cc4401e079d540d88f3fd76cce8ccb4
Hem <shaktimagan@gmail.com>
2022-10-25 18:11:03 +0530

    Update App.controller.js
    New controller changes

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,18 +1,38 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
-	"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
 
-	return Controller.extend("sikhoerp.controller.App", {
+    return Controller.extend("sikhoerp.controller.App", {
+      onInit: function () {
+        this.getView().byId("iconTabHeader").fireSelect();
+      },
+      onSelectTab: function (event) {
+        let sSelectedKey = event.getSource().getSelectedKey();
+        if (sSelectedKey == "COURSES") {
+          this.getView().byId("HomePanel").setVisible(false);
+          this.getView().byId("CoursesPanel").setVisible(true);
+          this.getView().byId("ContactPanel").setVisible(false);
+          this.getView().byId("homepageFooter").setVisible(false);
+          //
+        }
+        if (sSelectedKey == "HOME") {
+          this.getView().byId("HomePanel").setVisible(true);
+          this.getView().byId("CoursesPanel").setVisible(false);
+          this.getView().byId("ContactPanel").setVisible(false);
+          this.getView().byId("homepageFooter").setVisible(true);
+        }
 
-		onShowHello : function () {
-			
-		
-
-			// show message
-			MessageToast.show("Hi");
-		}
-	});
-
-});
+        if (sSelectedKey == "CONTACT") {
+          this.getView().byId("HomePanel").setVisible(false);
+          this.getView().byId("CoursesPanel").setVisible(false);
+          this.getView().byId("ContactPanel").setVisible(true);
+          this.getView().byId("homepageFooter").setVisible(false);
+        }
+      },
+      onLinkedinImagePress: function () {
+        alert("Tested");
+      },
+    });
+  }
+);
