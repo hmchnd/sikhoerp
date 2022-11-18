4e6212a48e49fb6fbe01a4c5dba1ebae60139037
Hem <shaktimagan@gmail.com>
2022-11-18 21:47:35 +0530

    Contact chages

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,8 +1,13 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/library",
+    "sap/ui/core/Fragment",
+  ],
+  function (Controller, MessageToast, mobileLibrary, Fragment) {
     "use strict";
-
+    var URLHelper = mobileLibrary.URLHelper;
     return Controller.extend("sikhoerp.controller.App", {
       onInit: function () {
         this.getView().byId("iconTabHeader").fireSelect();
@@ -32,6 +37,42 @@ sap.ui.define(
       },
       onLinkedinImagePress: function () {
         alert("Tested");
+      },
+      handleUrlPress: function (oEvent) {
+        var oButton = oEvent.getSource(),
+          oView = this.getView();
+
+        // create popover
+        if (!this._pPopover) {
+          this._pPopover = Fragment.load({
+            id: oView.getId(),
+            name: "sikhoerp.view.Popover",
+            controller: this,
+          }).then(function (oPopover) {
+            oView.addDependent(oPopover);
+
+            return oPopover;
+          });
+        }
+        this._pPopover.then(function (oPopover) {
+          oPopover.openBy(oButton);
+        });
+      },
+      onCall: function (evt) {
+        URLHelper.triggerTel(this._getVal(evt));
+      },
+      _getVal: function (evt) {
+        return evt.getSource().getValue();
+      },
+      onEmail: function (evt) {
+        URLHelper.triggerEmail(
+          this._getVal(evt),
+          "Info Request",
+          false,
+          false,
+          false,
+          true
+        );
       },
     });
   }
